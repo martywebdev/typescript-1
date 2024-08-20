d94668e7518af60eeb581faf21c6ca9f72239029
martywebdev <martywebdevelopment@gmail.com>
2024-08-20 08:41:04 +0800

    more classes and custom types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
 // Code goes here!
-//project state management
 
-class ProjectState {
-    private listeners: any[] = []
-    private projects: any[] = []
-    private static instance: ProjectState
-   
-
-    private constructor() {
+//project type
+enum ProjectStatus {
+    Active, Finished
+}
+class Project {
+    constructor(
+        public id:string, 
+        public title:string, 
+        public description:string, 
+        public people:number,
+        public status: ProjectStatus
+    ) {
 
     }
+}
+//project state management
+
+type Listener = (items:Project[]) => void
+class ProjectState {
+    private listeners: Listener[] = []
+    private projects: Project[] = []
+    private static instance: ProjectState   
+
+    private constructor() {}
 
     static getInstance() {
         if (this.instance) {
@@ -20,22 +34,19 @@ class ProjectState {
         return this.instance
     }
 
-    projectsList() {
-        // console.log(this.projects)
-    }
 
-    addListener(listenerFn: Function) {
+    addListener(listenerFn: Listener) {
         this.listeners.push(listenerFn)
     }
 
     addProject(title:string, description:string, numOfPeople:number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title:title,
-            description:description,
-            people:numOfPeople
-        }
-
+        const newProject = new Project(
+            Math.random().toString(),
+            title,
+            description, 
+            numOfPeople,
+            ProjectStatus.Active
+        )
         this.projects.push(newProject)
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice())//<-this.projects.slice was passed to projects list when addListeners was called
@@ -98,7 +109,7 @@ class ProjectList {
     templateElement:HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLElement
-    assignedProjects:any[]
+    assignedProjects:Project[]
 
     constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement
@@ -108,7 +119,7 @@ class ProjectList {
         this.element = importedNode.firstElementChild as HTMLElement 
         this.element.id = `${this.type}-projects`
         //this is the listeners to add the projects to the list
-        projectState.addListener((projects: any[]) => {
+        projectState.addListener((projects: Project[]) => {
             this.assignedProjects = projects
             this.renderProjects()
         })
@@ -119,9 +130,8 @@ class ProjectList {
     }
 
     private renderProjects() {
-        console.log(this.assignedProjects)
+        console.log('assigned', this.assignedProjects)
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
-        projectState.projectsList()
         for (const projectItem of this.assignedProjects) {
             const listItem = document.createElement('li')
             listItem.textContent = projectItem.title
